b4e326d0bd9fff0d085882e98d47a840f2bf6ebd
juanca005 <alessi.juan@gmail.com>
2020-11-30 20:03:26 -0300

    30/11

---
+++ DESARROLLO FINAL/Backup/V2 - 30NOV2020/librerias.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import cv2
+import time
+import logging
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import os
+import re
+import math
+import re
+import time
+import sys, getopt
+import os
+from os.path import basename
+from PIL import Image
+from urllib.request import urlopen
+import urllib.request
+import requests
+import array
+from concurrent.futures import ProcessPoolExecutor
+import io
+from os import remove
+import os, shutil
